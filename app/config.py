65256096ebf843ce9088ccb0acc6977429199847
Rock2Knight <f.merten@mail.ru>
2025-08-17 18:00:30 +0300

    test ci 15

--- app/config.py
+++ app/config.py
@@ -1,4 +1,6 @@
 import os
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -11,7 +13,7 @@ class Settings(BaseSettings):
     
     # DATABASE_SQLITE = 'sqlite+aiosqlite:///data/db.sqlite3'
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
+        env_file=Path(__file__).parent.parent / ".env"
     )
 
     def get_db_url(self):
